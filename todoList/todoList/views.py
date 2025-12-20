9cfc90821319c265ac929fbb6c053e1f64c9945d
Ganesh369-programmer <ganesh3692005@gmail.com>
2025-12-20 19:41:07 +0530

    fontent of Todo List

---
+++ todoList/todoList/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render , redirect
+
+def signup(request):
+    if request.method == 'POST':
+        fnm = request.POST.get('fnm')
+        emailid = request.POST.get('email')
+        pwd = request.POST.get('pwd')
+        
+        
+    return render(request , 'signup.html')
